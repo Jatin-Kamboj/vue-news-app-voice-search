d84b3f43164cd9cf1cee113003123e206487dd28
jatin-kamboj <er.jatinkamboj96@gmail.com>
2020-12-13 17:33:22 +0530

    Refacted code and adde utils functions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,29 @@
 <template>
-  <div class="test"><span class="test"> Home</span></div>
+  <div>Home</div>
 </template>
 
 <script>
-export default {};
+// import alanBtn from "@alan-ai/alan-sdk-web";
+
+export default {
+  beforeMount() {},
+  methods: {
+    initialiseAlan() {
+      // /**
+      //  * Integerates alan api and adds alan button
+      //  */
+      // alanBtn({
+      //   key: process.env.VUE_APP_ALAN_API_KEY,
+      //   onCommand: ({ command }) => {
+      //     // Call the client code that will react to the received command
+      //     if (command === "testcommand") {
+      //       alert("Played");
+      //     }
+      //   },
+      // });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
