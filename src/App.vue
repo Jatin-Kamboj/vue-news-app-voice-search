94db1b55ee01042d46dafd4e8c515341865e5ee7
jatin-kamboj <er.jatinkamboj96@gmail.com>
2020-12-13 17:18:58 +0530

    Added speechSynthesis api

--- src/App.vue
+++ src/App.vue
@@ -9,19 +9,36 @@
 </template>
 
 <script>
-import alanBtn from "@alan-ai/alan-sdk-web";
+// import alanBtn from "@alan-ai/alan-sdk-web";
 import "@/assets/styles/main.scss";
 
 export default {
   beforeMount() {
-    alanBtn({
-      key: process.env.VUE_APP_ALAN_API_KEY,
-      onCommand: (commandData) => {
-        if (commandData.command === "go:back") {
-          // Call the client code that will react to the received command
-        }
-      },
-    });
+    if ("speechSynthesis" in window) {
+      // Speech Synthesis supported 🎉
+      var msg = new SpeechSynthesisUtterance();
+      msg.text = "Good Morning";
+      window.speechSynthesis.speak(msg);
+    } else {
+      // Speech Synthesis Not Supported 😣
+      alert("Sorry, your browser doesn't support text to speech!");
+    }
+  },
+  methods: {
+    initialiseAlan() {
+      // /**
+      //  * Integerates alan api and adds alan button
+      //  */
+      // alanBtn({
+      //   key: process.env.VUE_APP_ALAN_API_KEY,
+      //   onCommand: ({ command }) => {
+      //     // Call the client code that will react to the received command
+      //     if (command === "testcommand") {
+      //       alert("Played");
+      //     }
+      //   },
+      // });
+    },
   },
 };
 </script>
