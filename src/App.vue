72bd62c67ce62b2f78845a5479365631979bc760
jatin-kamboj <er.jatinkamboj96@gmail.com>
2020-12-14 01:35:05 +0530

    Added header component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <!------------------------  Vue Router  -------------------------->
+  <Header />
+  <!-- Vue Router -->
   <router-view />
 </template>
 
 <script>
+import { Header } from "@/components/layout";
 import "@/assets/styles/main.scss";
 
-export default {};
+export default {
+  components: {
+    Header,
+  },
+};
 </script>
 
 <style lang="scss"></style>
